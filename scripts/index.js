2551259d001370b2c52afbf6e04864bf76497d94
Ro0kly <snallytango@yandex.ru>
2024-08-06 02:49:45 +0300

    main / add cards and create delete method

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,45 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.getElementById("card-template");
 
-// @todo: Функция создания карточки
+// DOM узлы
 
-// @todo: Функция удаления карточки
+const cardsContainer = document.querySelector(".places__list");
+const addCardButton = document.querySelector(".profile__add-button");
 
-// @todo: Вывести карточки на страницу
+// Функция создания карточки
+
+const createCard = ({ name, link }, callback) => {
+  const cardTemplateContent = cardTemplate.content;
+  const cardNode = cardTemplateContent.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardNode.querySelector(".card__image");
+  const cardTitle = cardNode.querySelector(".card__title");
+  const removeButton = cardNode.querySelector(".card__delete-button");
+
+  cardImage.src = link;
+  cardImage.alt = `${name}`;
+
+  cardTitle.textContent = name;
+
+  removeButton.addEventListener('click', callback)
+
+  return cardNode;
+};
+
+// Функция удаления карточки
+
+const removeCard = (e) => {
+  const card = e.target.closest(".card");
+  card.remove();
+};
+
+// Вывести карточки на страницу
+
+const initCards = (cards) => {
+  cards.forEach((card) => {
+    cardsContainer.append(createCard(card, removeCard));
+  });
+};
+
+initCards(initialCards);
